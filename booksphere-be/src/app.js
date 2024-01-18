36951f227bd28da2f6d7bce665ccac8979bd05bb
Kamal Bijaraniyan <52608133+mfluckyxd@users.noreply.github.com>
2024-01-18 16:59:55 +0530

    project set-up and authentication routes

---
+++ booksphere-be/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const authRouter = require("./routes/auth");
+// const blogRouter = require("./routes/blog");
+const cors = require("cors");
+
+// to enable cross origin communication
+app.use(cors());
+// to read the bod from client
+app.use(express.json());
+app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/blog", blogRouter);
+
+module.exports = app;
